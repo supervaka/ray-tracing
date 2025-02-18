eaaecf4a14ec45bb448ce8d58c9d06692302aabc
supervaka <isaac96kim@gmail.com>
2025-02-18 12:34:58 -0500

    ch11 Positionable Camera

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ mod ray;
 mod sphere;
 mod vec3;
 
-use std::{io, rc::Rc};
+use std::io;
+use std::rc::Rc;
 
 use camera::Camera;
 use color::Color;
@@ -17,7 +18,7 @@ use hittable_list::HittableList;
 use material::{Dielectric, Lambertian, Metal};
 use ray::Ray;
 use sphere::Sphere;
-use vec3::Point3;
+use vec3::{Point3, Vec3};
 
 fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
     // If we've exceeded the ray bounce limit, no more light is gathered
@@ -26,7 +27,7 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
     }
 
     let mut rec = HitRecord::new();
-    if world.hit(r, 0.001, f64::INFINITY, &mut rec) {
+    if world.hit(r, 0.001, common::INFINITY, &mut rec) {
         let mut attenuation = Color::default();
         let mut scattered = Ray::default();
         if rec
@@ -37,6 +38,7 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
         {
             return attenuation * ray_color(&scattered, world, depth - 1);
         }
+        return Color::new(0.0, 0.0, 0.0);
     }
 
     let unit_direction = vec3::unit_vector(r.direction());
@@ -58,9 +60,9 @@ fn main() {
     let mut world = HittableList::new();
 
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Dielectric::new(1.5));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
     let material_left = Rc::new(Dielectric::new(1.5));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Box::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
@@ -75,6 +77,11 @@ fn main() {
     world.add(Box::new(Sphere::new(
         Point3::new(-1.0, 0.0, -1.0),
         0.5,
+        material_left.clone(),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.45,
         material_left,
     )));
     world.add(Box::new(Sphere::new(
@@ -85,7 +92,13 @@ fn main() {
 
     // Camera
 
-    let cam = Camera::new();
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+        ASPECT_RATIO,
+    );
 
     // Render
 
--- src/material.rs
+++ src/material.rs
@@ -1,7 +1,7 @@
 use crate::color::Color;
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
-use crate::vec3;
+use crate::{common, vec3};
 
 pub trait Material {
     fn scatter(
@@ -84,6 +84,13 @@ impl Dielectric {
             ir: index_of_refraction,
         }
     }
+
+    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        // Use Schlick's approximation for reflectance
+        let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+        r0 = r0 * r0;
+        r0 + (1.0 - r0) * f64::powf(1.0 - cosine, 5.0)
+    }
 }
 
 impl Material for Dielectric {
@@ -101,10 +108,20 @@ impl Material for Dielectric {
         };
 
         let unit_direction = vec3::unit_vector(r_in.direction());
-        let refracted = vec3::refract(unit_direction, rec.normal, refraction_ratio);
+        let cos_theta = f64::min(vec3::dot(-unit_direction, rec.normal), 1.0);
+        let sin_theta = f64::sqrt(1.0 - cos_theta * cos_theta);
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction = if cannot_refract
+            || Self::reflectance(cos_theta, refraction_ratio) > common::random_double()
+        {
+            vec3::reflect(unit_direction, rec.normal)
+        } else {
+            vec3::refract(unit_direction, rec.normal, refraction_ratio)
+        };
 
         *attenuation = Color::new(1.0, 1.0, 1.0);
-        *scattered = Ray::new(rec.p, refracted);
+        *scattered = Ray::new(rec.p, direction);
         true
     }
 }
